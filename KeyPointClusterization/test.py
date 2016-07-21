b174c4fe208032e7310129e64afc9547220d6493
CherkashinSergey <4erkashin@list.ru>
2016-07-21 16:38:53 +0300

    Sync

--- KeyPointClusterization/test.py
+++ KeyPointClusterization/test.py
@@ -1,9 +1,3 @@
-﻿import cv2
-import os
-
-ROOT_Dir = 'D:\\SCherkashin\\TrainingFolder\\Test\\'
-new_Dir = 'new'
-os.chdir(ROOT_Dir)
-img = cv2.imread('1.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite(new_Dir + '\\' + '2.jpg', img)
+﻿s = [1,2]
+s1 = (s,)
+print()
